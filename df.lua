cb588a22786c9b79b67ad0b87591136334ae82e7
ZuprizeDev <100492593+ZuprizeDev@users.noreply.github.com>
2023-01-09 16:31:32 +0000

    Create df.lua

---
+++ df.lua
@@ -0,0 +1,115 @@
+local AspectRemoteHooking = {}
+
+local ReturnTable = {}
+
+--// Single Invokes Hooking Keys \\--
+_G.Invoke = game:GetService("HttpService"):GenerateGUID(false)
+_G.OldInvoke = _G.Invoke
+
+--// Single Remotes Hooking Keys \\--
+_G.Remote = game:GetService("HttpService"):GenerateGUID(false)
+_G.OldRemote = _G.Remote
+
+--// All Invokes Hooking Keys \\--
+_G.AllInvoke = game:GetService("HttpService"):GenerateGUID(false)
+_G.OldAllInvoke = _G.AllInvoke
+
+--// All Remotes Hooking Keys \\--
+_G.AllRemote = game:GetService("HttpService"):GenerateGUID(false)
+_G.OldAllRemote = _G.AllRemote
+
+function AspectRemoteHooking:GenerateNewInvokeKey()
+    _G.OldInvoke = _G.Invoke
+    _G.Invoke = game:GetService("HttpService"):GenerateGUID(false)
+    return _G.Invoke
+end
+
+function AspectRemoteHooking:GenerateNewRemoteKey()
+    _G.OldRemote = _G.Remote
+    _G.Remote = game:GetService("HttpService"):GenerateGUID(false)
+    return _G.Remote
+end
+
+function AspectRemoteHooking:GenerateNewAllInvokeKey()
+    _G.OldAllInvoke = _G.AllInvoke
+    _G.AllRemote = game:GetService("HttpService"):GenerateGUID(false)
+    return _G.AllInvoke
+end
+
+function AspectRemoteHooking:GenerateNewAllRemoteKey()
+    _G.OldAllRemote = _G.AllRemote
+    _G.AllRemote = game:GetService("HttpService"):GenerateGUID(false)
+    return _G.AllRemote
+end
+
+function HandleHookedInfo(hookData)
+	spawn(function()
+        coroutine.resume(coroutine.create(function()
+            spawn(function()
+				for i,v in pairs(hookData) do
+					table.insert(ReturnTable, v)
+				end
+			
+				return ReturnTable
+			end)
+		end))
+	end)
+end
+
+function AspectRemoteHooking:GetReturnTable()
+	local OldTable = ReturnTable
+	local NewTable = {["RemoteName"] = "RemoteName"}
+	ReturnTable = NewTable
+
+	return OldTable
+end
+
+function AspectRemoteHooking:hookInvoke(RemoteName, WhatToHook)
+    local a
+    a = hookfunction(WhatToHook, function(...)
+		if _G.Invoke == _G.OldInvoke then
+			HandleHookedInfo({...})
+			return a(...)
+		else
+			return a(...)
+		end
+    end)
+end
+
+function AspectRemoteHooking:hookFire(RemoteName, WhatToHook)
+	local a
+    a = hookfunction(WhatToHook, function(...)
+		if _G.Remote == _G.OldRemote then
+			HandleHookedInfo({...})
+			return a(...)
+		else
+			return a(...)
+		end
+    end)
+end
+
+function AspectRemoteHooking:hookAllInvokes(WhatToHook)
+	local a
+    a = hookfunction(WhatToHook, function(...)
+		if _G.AllInvoke == _G.OldAllInvoke then
+			HandleHookedInfo({...})
+			return a(...)
+		else
+			return a(...)
+		end
+    end)
+end
+
+function AspectRemoteHooking:hookAllFires(WhatToHook)
+    local a
+    a = hookfunction(WhatToHook, function(...)
+		if _G.AllRemote == _G.OldAllRemote then
+			HandleHookedInfo({...})
+			return a(...)
+		else
+			return a(...)
+		end
+    end)
+end
+
+return AspectRemoteHooking
